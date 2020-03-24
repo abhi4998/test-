d1170516cf387c05e2a5a13d426a804db1daf60e
abhi4998 <43167663+abhi4998@users.noreply.github.com>
2020-03-24 07:46:29 +0530

    Add files via upload

---
+++ 2.1.sum.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a=20;
+    char b='m';
+    float c=89.55;
+    printf("hi, i am %d year old and my weight is %f or my gender is %c",a,c,b);
+
+}
